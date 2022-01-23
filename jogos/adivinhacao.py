2c1d96f58673a39c1229c7348d6a6b8b525b7fd8
Renato Veneroso <rveneroso@gmail.com>
2022-01-22 23:31:56 -0300

    Esse commit contém uma série de evoluçoes no código

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,11 +1,29 @@
+import random
+
 print("********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
-numero_secreto = 32
-total_tentativas = 3
+# Geração de um número aleatório entre 0.0 e 1.0. A multiplicação por 100 e o round são para tornar o número
+# aleatório em um inteiro. Mas há um problema nessa abordagem: se o número aleatório for 0.0, o inteiro gerado
+# será 0, que é inválido para o contexto do programa.
+# numero_secreto = round(random.random() * 100)
+#
+# Assim sendo, o melhor é utilizar a função randrange:
+numero_secreto = random.randrange(1,101)
+# Lembrando que o segundo parâmetro é exclusivo: 101 não faz parte da faixa aceita. O último valor aceito é 100.
+total_tentativas = 0
 rodada = 1
+pontos = 1000
 
+print("Escolha o nível de dificuldade: (1) Fácil (2) Médio (3) Difícil")
+nivel = int(input("Faça sua escolha:"))
+if(nivel == 1):
+    total_tentativas = 20
+elif(nivel == 2):
+    total_tentativas = 10
+else:
+    total_tentativas = 5
 # Sintaxe do loop usando comando while
 # while(rodada <= total_tentativas):
 #
@@ -17,12 +35,16 @@ for rodada in range(1, total_tentativas+1):
     # Usando String interpolation
     print("Tentativa {} de {}".format(rodada, total_tentativas))
     # Por padrão quando se usa o input a variável que receberá o valor vindo do input será do tipo str
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
 
     print("Você digitou o número ",chute_str)
 
     # Converte o valor de chute_str para uma variável do tipo int
     chute = int(chute_str)
+
+    if(chute < 0 or chute > 100):
+        print("O número informado está fora da faixa permitida")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -35,13 +57,20 @@ for rodada in range(1, total_tentativas+1):
         # devidamente identado. O primeiro comando que não estiver devidamente identado será considerado como fora
         # do bloco if / else.
         #
-        print("Você acertou!")
+        print("Você acertou! Você fez {} pontos".format(pontos))
+        break
     else:
         print("Você errou!")
         if(maior):
             print("O número que você digitou é maior que o número secreto.")
+            if (rodada == total_tentativas):
+                print("O número secreto era {}. Você fez {} pontos!".format(numero_secreto, pontos))
         elif(menor):
             print("O número que você digitou é menor que o número secreto.")
+            if (rodada == total_tentativas):
+                print("O número secreto era {}. Você fez {} pontos!".format(numero_secreto, pontos))
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
     # No caso de loop usando o comando for não se deve somar 1 ao valor da variável 'rodada'
     # rodada = rodada + 1
 
